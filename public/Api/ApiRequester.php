11f7001fd2586b131867ae202fd12638a01a57b6
=Austin Antoine <=austinantoine@hotmail.com>
2019-03-02 17:15:03 -0500

    Make api classes

---
+++ public/Api/ApiRequester.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aantoine97
+ * Date: 2019-03-02
+ * Time: 5:05 PM
+ */
+
+class ApiRequester
+{
+    public static function getNews($location)
+    {
+
+    }
+}
---
+++ public/Api/ApiUri.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aantoine97
+ * Date: 2019-03-02
+ * Time: 5:07 PM
+ */
+define("NEWS_URL_BEGINNING", "https://newsapi.org/v2/everything?q=");
+define("NEWS_URL_END", "&from=2019-02-02&sortBy=publishedAt&apiKey=0c865f9e95dc4d67ab00616448c75638");
