ff4533c4d2020491957397a04aba5814dc876229
=Austin Antoine <=austinantoine@hotmail.com>
2019-03-02 18:49:51 -0500

    Add get methods for air and news apis

--- public/Api/ApiRequester.php
+++ public/Api/ApiRequester.php
@@ -9,13 +9,17 @@ use ApiUri;
 
 class ApiRequester
 {
-    public static function getNews($location)
+    public static function getNews()
     {
-
+        $url = NEWS_URL . "q=enviroment&startDate=" . date("Y/m/d") . "&sortBy=publishedAt&apiKey=" . AIR_APIKEY;
+        $json = file_get_contents($url);
+        return json_decode($json, true);
     }
 
     public static function getAirQuality($country, $state, $city)
     {
-
+        $url = AIR_URL . "country=" . $country . "&state=" . $state . "&city=" . $city . "&key=" . AIR_APIKEY;
+        $json = file_get_contents($url);
+        return json_decode($json, true);
     }
 }
