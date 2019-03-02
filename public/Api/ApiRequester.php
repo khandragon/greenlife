16466d6551b36c065478d34fba93a7c011408e17
=Austin Antoine <=austinantoine@hotmail.com>
2019-03-02 17:41:01 -0500

    Add constants

--- public/Api/ApiRequester.php
+++ public/Api/ApiRequester.php
@@ -5,10 +5,16 @@
  * Date: 2019-03-02
  * Time: 5:05 PM
  */
+use ApiUri;
 
 class ApiRequester
 {
     public static function getNews($location)
+    {
+
+    }
+
+    public static function getAirQuality($country, $state, $city)
     {
 
     }
