bd3a5c0da284fbfb8b925eeb1156aeec83aa49b2
Alex Kofman <akofman123@gmail.com>
2019-03-03 02:55:28 -0500

    Retreive average temperature given a Climate ID, date, month, year, province

--- public/index.php
+++ public/index.php
@@ -22,9 +22,15 @@ function displayNews(){
   }
 }
 
-function parseCSV($date){
+function parseCSV($climateId,$date,$month,$year,$province){
   $row = 1;
-  $tempLink = "https://dd.meteo.gc.ca/climate/observations/daily/csv/QC/climate_daily_QC_7085106_2018-07_PID.csv";
+
+  if(strlen($month) == 1){
+    echo 'true';
+    $month = str_pad($month, 2, '0', STR_PAD_LEFT);
+  }
+  $tempLink = "https://dd.meteo.gc.ca/climate/observations/daily/csv/". $province."/climate_daily_".$province."_".$climateId."_".$year."-". $month ."_PID.csv";
+  echo $tempLink.'</br>';
   if (($handle = fopen($tempLink, "r")) !== FALSE) {
       while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
           $num = count($data);
@@ -34,6 +40,10 @@ function parseCSV($date){
           }
           if($row > 27){
             if($data[3] == $date){
+              if($data[5] == 'M' || $data[7] == 'M' || $data[5] == '' || $data[7] == '' ){
+                echo '<p>There is no recorded temperature for this</p>';
+                break;
+              }
               $meanTemp = ($data[5] + $data[7]) / 2;
               echo "<p>Your mean temperature for that day is: " .$meanTemp . "</p>\n";
               $row++;
@@ -46,7 +56,8 @@ function parseCSV($date){
 
 if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['city']) && isset($_GET['country']) && isset($_GET['state']))
 {
-  parseCSV(3);
+  //Climate ID, date, month, year
+  parseCSV(7011982,9,2,2018,'QC');
 
   // $country = $_GET['country'];
   // $state = $_GET['state'];
