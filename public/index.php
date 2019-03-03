99b1c3f0ced0349d9cdc3e8d6e0fe725fc7470c7
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2019-03-02 21:10:46 -0500

    Add one page implementation

--- public/index.php
+++ public/index.php
@@ -3,10 +3,27 @@
         <meta charset="utf-8">
         <title>GreenLife</title>
         <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+        <link rel="stylesheet" href="resources/css/styles.css">    
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-        <a class="navbar-brand" href="#">Navbar</a>
-        </nav>
+        <header id="header-bg" class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col">
+                        <div class="row align-items-center justify-content-center text-center">
+                            <img src="resources/images/logo-circle.png" alt="GreenLife logo" id="header-logo">
+                        </div>
+                        <div class="row align-items-center justify-content-center text-center">
+                            <h5 class="display-4">GreenLife</h3>
+                        </div>
+                        <div class="row justify-content-center mt-5">
+                            <a href="#" class="mt-1">
+                                <img src="resources/images/thin-arrowheads-pointing-down.png" alt="arrow-logo" id="arrow-logo">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     </body>
 </html>
