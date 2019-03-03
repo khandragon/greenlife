86aa7d7cc329ff086d170e4e14f68e6cf0432998
=Austin Antoine <=austinantoine@hotmail.com>
2019-03-03 10:45:06 -0500

    Add air quality implementation

--- public/air.php
+++ public/air.php
@@ -1,27 +1,3 @@
-<?php
-function displayWeather($lat,$long)
-{
-  $result = ApiRequester::getAirQuality($lat, $long);
-  $info = ApiRequester::getInfo($lat, $long);
-  $currentData = $result['data']['current'];
-  $humidity = $currentData['weather']['hu'];
-  $aqi = $currentData['pollution']['aqius'];
-  $temperature =  $currentData['weather']['tp'];
-
-  echo '<h2>Humidity: '.$humidity.'</h2>';
-  echo '<h2>Air Quality Index: '. $aqi .' </h2>';
-  echo '<h2>Temperature(celsius): '. $temperature .' </h2>';
-  echo '<h2>Province: '. $info["state"] .' </h2>';
-  echo '<h2>City: '. $info["city"] .' </h2>';
-}
-
-if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['lat']) && isset($_GET['long']))//isset($_GET['city']) && isset($_GET['country']) && isset($_GET['state']))
-{
-  $lat = $_GET['lat'];
-  $long = $_GET['long'];
-  displayWeather($lat,$long);
-}
-?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -48,15 +24,67 @@ if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['lat']) && isset($_GET['l
                     </ul>
                 </div>
             </div>
-        </nav> 
-        <div id="search">
-      <form id="frm1" action="" method="get">
-        <input type="hidden" id="latSubmit" name="lat" value="">
-        <input type="hidden" id="longSubmit" name="long" value="">
-        <input type="submit">
-        <div id="map"></div>
-      </form>
-    </div>
+        </nav>
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col mt-5">
+                        <div class="row align-items-center justify-content-center text-center">
+                            <h1 class="display-3 mb-5">Air Quality Checkup 🔍</h1>
+                        </div>
+                        <div class="row align-items-center justify-content-center text-center">
+                            <form id="frm1" action="" method="get" class="w-100">
+                                <input type="hidden" id="latSubmit" name="lat" value="">
+                                <input type="hidden" id="longSubmit" name="long" value="">
+                                <div class="form-row align-items-center justify-content-center text-center mb-5">
+                                    <button type="submit" class="btn btn-outline-primary btn-lg">Search a location</button>
+                                </div>
+                                <div id="map"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <?php
+        include 'resources/Api/ApiRequester.php';
+
+        function displayWeather($lat,$long)
+        {
+            $result = ApiRequester::getAirQuality($lat, $long);
+            $info = ApiRequester::getInfo($lat, $long);
+            $currentData = $result['data']['current'];
+            $humidity = $currentData['weather']['hu'];
+            $aqi = $currentData['pollution']['aqius'];
+            $temperature =  $currentData['weather']['tp'];
+
+            echo '<div class="container-fluid mb-5">';
+            echo '<div class="row align-items-center justify-content-center text-center">';
+            echo '<div class="col"><p class="h4">Results</p></div>';
+            echo '<div class="w-100"></div>';
+            echo '<div class="col"><p class="h4">Humidity</p></div>';
+            echo '<div class="col"><p class="h4">'.$humidity.'</p></div>';
+            echo '<div class="w-100"></div>';
+            echo '<div class="col"><p class="h4">Air Quality Index</p></div>';
+            echo '<div class="col"><p class="h4">'.$aqi.'</p></div>';
+            echo '<div class="w-100"></div>';
+            echo '<div class="col"><p class="h4">Temperature(celsius)</p></div>';
+            echo '<div class="col"><p class="h4">'.$temperature.' C</p></div>';
+            echo '<div class="w-100"></div>';
+            echo '<div class="col"><p class="h4">Province</p></div>';
+            echo '<div class="col"><p class="h4">'.$info["state"].'</p></div>';
+            echo '<div class="w-100"></div>';
+            echo '<div class="col"><p class="h4">City</p></div>';
+            echo '<div class="col"><p class="h4">'.$info["city"].'</p></div></div></div>';
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['lat']) && isset($_GET['long']))//isset($_GET['city']) && isset($_GET['country']) && isset($_GET['state']))
+        {
+            $lat = $_GET['lat'];
+            $long = $_GET['long'];
+            displayWeather($lat,$long);
+        }
+        ?>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVzscNT6-Aj5tXOPqS6eC_-Zj8kE0N1lY&callback=initMap"></script>
     </body>
 </html>
