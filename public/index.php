978ce6df5639c57bdfa20165bee27bb414410803
kwan.s <stephen.kwan@dawsoncollege.qc.ca>
2019-03-03 01:02:13 -0500

    Add offers section

--- public/index.php
+++ public/index.php
@@ -13,11 +13,15 @@
                         <div class="row align-items-center justify-content-center text-center">
                             <img src="resources/images/logo-circle.png" alt="GreenLife logo" id="header-logo">
                         </div>
-                        <div class="row align-items-center justify-content-center text-center">
-                            <h5 class="display-4">GreenLife</h3>
+                        <div class="row align-items-center justify-content-center text-center mt-5">
+                            <h1>GreenLife</h1>
+                        </div>
+                        <hr class="my-4">
+                        <div class="row align-items-center justify-content-center text-center mt-4">
+                            <h4>Learn more about the environment with us</h4>
                         </div>
                         <div class="row justify-content-center mt-5">
-                            <a href="#search-section" class="mt-5">
+                            <a href="#offers" class="mt-5">
                                 <img src="resources/images/thin-arrowheads-pointing-down.png" alt="arrow-logo" id="arrow-logo">
                             </a>
                         </div>
@@ -25,8 +29,53 @@
                 </div>
             </div>
         </header>
-        <section id="search-section" class="page-section">
-            <div style="height:100%"></div>
+        <section id="offers" class="mt-5 mb-5">
+            <div class="container">
+                <h2 class="text-center">Our offers</h2>
+                <hr class="my-4">
+                <div class="row justify-content-center text-center">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="resources/images/cloud.png" alt="cloud">
+                            <h3 class="mb-2">Weather Data</h3>
+                            <p class="text-muted mb-0">We provide weather information with Canada's latest statistics.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="resources/images/wind.png" alt="wind">
+                            <h3 class="mb-2">Air Quality</h3>
+                            <p class="text-muted mb-0">We make sure to inform the quality of air you are breathing.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img src="resources/images/heart.png" alt="heart">
+                            <h3 class="mb-2">Open source</h3>
+                            <p class="text-muted mb-0">This project is made with passion and we would love to share it with everyone.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
+        <!-- <section id="search" class="page-section">
+            <div class="container h-100">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Enter year">
+                        <small class="form-text text-muted">Year</small>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Enter month">
+                        <small class="form-text text-muted">Month</small>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Enter day">
+                        <small class="form-text text-muted">Day</small>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </section> -->
     </body>
 </html>
